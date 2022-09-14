4068d940d65d163ab0986873de802f652ede778b
stevenbehrens <bobgiblets@gmail.com>
2022-09-14 08:32:40 -0400

    Added initials 2

--- Assignment1.java
+++ Assignment1.java
@@ -6,6 +6,18 @@ static Scanner input = new Scanner(System.in);
 
 
     public static void main(String[] args) {
+
+        System.out.println("SSSSSSSSSSSSSSSSSS  BBBBBBBBBBBBBB");
+        System.out.println("SSSSSSSSSSSSSSSSSS  BBB         BBB");
+        System.out.println("SSSSSSSS            BBB          BBB");
+        System.out.println("SSSSSSSS            BBB          BBB");
+        System.out.println("SSSSSSSSSSSSSSSSSS  BBBBBBBBBBBBB    ");
+        System.out.println("SSSSSSSSSSSSSSSSSS  BBBBBBBBBBBBB    ");
+        System.out.println("         SSSSSSSSS  BBB          BBB");
+        System.out.println("         SSSSSSSSS  BBB          BBB");
+        System.out.println("SSSSSSSSSSSSSSSSSS  BBB         BBB ");
+        System.out.println("SSSSSSSSSSSSSSSSSS  BBBBBBBBBBBBBB  ");
+
         System.out.println("Type something");
         String name;
         name = input.next();
