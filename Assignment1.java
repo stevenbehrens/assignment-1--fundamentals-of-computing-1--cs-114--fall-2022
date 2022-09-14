0b8e0942945ec3d29bfa38c5d7421c1260f4eeb7
stevenbehrens <bobgiblets@gmail.com>
2022-09-14 08:26:35 -0400

    Added initials

--- Assignment1.java
+++ Assignment1.java
@@ -4,6 +4,7 @@ public class Assignment1 {
 static Scanner input = new Scanner(System.in);
 
 
+
     public static void main(String[] args) {
         System.out.println("Type something");
         String name;
