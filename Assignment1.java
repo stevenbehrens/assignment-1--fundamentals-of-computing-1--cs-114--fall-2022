c178b06ba25267d8b91f6be2a86d65fff38bc81d
stevenbehrens <bobgiblets@gmail.com>
2022-09-14 08:13:14 -0400

    Added scanner input

--- Assignment1.java
+++ Assignment1.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Assignment1 {
+static Scanner input = new Scanner(System.in);
+
+
     public static void main(String[] args) {
         // Add your code here
     }
